9c89da6479a62559fc6d5072f0343dbe50f96a33
Nat-liquido <nataliasantinello@liquido.com>
2022-11-25 11:26:04 -0300

    Changing nomenclatures.

--- Model/Brl/LiquidoBrlPaymentMethod.php
+++ Model/Brl/LiquidoBrlPaymentMethod.php
@@ -6,5 +6,5 @@ use \Magento\Payment\Model\Method\AbstractMethod;
 
 class LiquidoBrlPaymentMethod extends AbstractMethod {
 
-    protected $_code = 'liquidobrl';
+    protected $_code = 'liquido';
 }
