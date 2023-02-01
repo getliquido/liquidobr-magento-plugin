293db997d72b3398082676d1a86136b8a12620a6
Gervásio Araújo <gervasioaraujo@liquido.com>
2023-02-01 17:20:19 -0300

    added redirect to next steps after the layout changings

--- Util/Co/LiquidoCoPayInMethod.php
+++ Util/Co/LiquidoCoPayInMethod.php
@@ -5,18 +5,20 @@ namespace Liquido\PayIn\Util\Co;
 abstract class LiquidoCoPayInMethod
 {
     public const PSE = [
-        "title" => "PSE",
+        "id" => "pse",
+        "title" => "Transferencia bancaria",
         "description" => "El pago puede ser aprobado al instante.",
         "image" => "Liquido_PayIn::images/pse.png"
     ];
     public const CASH = [
+        "id" => "cash",
         "title" => "Pago en efectivo",
         "description" => "El pago puede ser aprobado al instante.",
         "image" => "Liquido_PayIn::images/efecty.png"
     ];
-    // public const CREDIT_CARD = [
-    //     "title" => "Tarjeta de crédito",
-    //     "description" => "El pago puede ser aprobado al instante.",
-    //     "image" => "Liquido_PayIn::images/credit-card.png"
-    // ];
+// public const CREDIT_CARD = [
+//     "title" => "Tarjeta de crédito",
+//     "description" => "El pago puede ser aprobado al instante.",
+//     "image" => "Liquido_PayIn::images/credit-card.png"
+// ];
 }
--- Util/Common/LiquidoPayInMethod.php
+++ Util/Common/LiquidoPayInMethod.php
@@ -5,6 +5,7 @@ namespace Liquido\PayIn\Util\Common;
 abstract class LiquidoPayInMethod
 {
     public const CREDIT_CARD = [
+        "id" => "credit-card",
         "title" => "Cartão de Crédito",
         "description" => "O pagamento poderá ser aprovado na hora.",
         "image" => "Liquido_PayIn::images/credit-card.png"
