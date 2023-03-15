fc3f79df6557034e66951ccc6c853feab34e8511
Nat-liquido <nataliasantinello@liquido.com>
2023-03-15 17:16:58 -0300

    Implementing creation of paid invoice and refund credit memo upon receipt of webhook notification.

--- Model/LiquidoWebhook.php
+++ Model/LiquidoWebhook.php
@@ -4,9 +4,17 @@ namespace Liquido\PayIn\Model;
 
 use \Magento\Framework\Webapi\Rest\Request;
 use \Magento\Framework\DataObject;
+use \Magento\Sales\Model\RefundOrder;
+use \Magento\Framework\App\ObjectManager;
+use \Magento\Sales\Model\Service\InvoiceService;
+use \Magento\Sales\Model\Order\Invoice;
+use \Magento\Framework\DB\Transaction;
+use \Psr\Log\LoggerInterface;
 
 use \Liquido\PayIn\Helper\LiquidoSalesOrderHelper;
 use \Liquido\PayIn\Helper\LiquidoSendEmail;
+
+use \LiquidoBrl\PayInPhpSdk\Util\Colombia\PaymentMethod;
 
 class LiquidoWebhook
 {
@@ -14,15 +22,29 @@ class LiquidoWebhook
 	private Request $request;
 	private LiquidoSalesOrderHelper $liquidoSalesOrderHelper;
 	private LiquidoSendEmail $sendEmail;
+	private RefundOrder $refundOrder;
+	private ObjectManager $objectManager;
+	private InvoiceService $invoiceService;
+	private Transaction $transaction;
+	private LoggerInterface $logger;
 
 	public function __construct(
 		Request $request,
 		LiquidoSalesOrderHelper $liquidoSalesOrderHelper,
-		LiquidoSendEmail $sendEmail
+		LiquidoSendEmail $sendEmail,
+		RefundOrder $refundOrder,
+		InvoiceService $invoiceService,
+		Transaction $transaction,
+		LoggerInterface $logger
 	) {
 		$this->request = $request;
 		$this->liquidoSalesOrderHelper = $liquidoSalesOrderHelper;
 		$this->sendEmail = $sendEmail;
+		$this->refundOrder = $refundOrder;
+		$this->invoiceService = $invoiceService;
+		$this->transaction = $transaction;
+		$this->logger = $logger;
+		$this->objectManager = ObjectManager::getInstance();
 	}
 
 	/**
@@ -30,6 +52,8 @@ class LiquidoWebhook
 	 */
 	public function processLiquidoCallbackRequest()
 	{
+		$className = static::class;
+		$this->logger->info("###################### BEGIN {$className} processLiquidoCallbackRequest ######################");
 
 		// *** TO DO: get headers from request
 
@@ -50,8 +74,32 @@ class LiquidoWebhook
 		if ($liquidoSalesOrderAlreadyExists) {
 
 			// if "transferStatus" not in $body { do something... }
-			$transferStatus = $body["data"]["chargeDetails"]["transferStatus"];
+			$transferStatus = $this->isRefund($eventType) ? 'REFUNDED' : $body["data"]["chargeDetails"]["transferStatus"];
 			$paymentMethod = $body["data"]["chargeDetails"]["paymentMethod"];
+
+			if ($transferStatus == 'REFUNDED') {
+				$this->logger->info("*************************** START REFUND *******************************");
+				$this->executeRefundOrder($orderId);
+			} else {
+				$order = $this->objectManager->create('\Magento\Sales\Model\Order')->load($orderId);
+				if ($order->canInvoice()) {
+					$this->logger->info("*************************** CREATE INVOICE *******************************");
+
+					$invoice = $this->invoiceService->prepareInvoice($order);
+					$invoice->setRequestedCaptureCase(Invoice::CAPTURE_OFFLINE);
+					$invoice->register();
+					$invoice->save();
+
+					$transactionSave = $this->transaction
+						->addObject($invoice)
+						->addObject($invoice->getOrder());
+					$transactionSave->save();
+
+					$order->addStatusHistoryComment(__('Invoice #' . $invoice->getIncrementId() . ' created automatically'))
+						->setIsCustomerNotified(false)
+						->save();
+				}
+			}
 
 			$orderData = new DataObject(array(
 				"orderId" => $orderId,
@@ -61,18 +109,31 @@ class LiquidoWebhook
 			));
 
 			$this->liquidoSalesOrderHelper->createOrUpdateLiquidoSalesOrder($orderData);
-		}
 
-		$params = array(
-			'name' => $body['data']['chargeDetails']['payer']['name'], 
-			'email' => $body['data']['chargeDetails']['payer']['email'],
-			'cashCode' => $body['data']['chargeDetails']['transferDetails']['payCash']['referenceNumber'], 
-			'statusCode' => $body['data']['chargeDetails']['transferStatusCode']);
-		$this->sendEmail->sendEmail($params, true);
+			if ($paymentmethod == PaymentMethod::CASH) {
+				$params = array(
+					'name' => $body['data']['chargeDetails']['payer']['name'],
+					'email' => $body['data']['chargeDetails']['payer']['email'],
+					'cashCode' => $body['data']['chargeDetails']['transferDetails']['payCash']['referenceNumber'],
+					'statusCode' => $body['data']['chargeDetails']['transferStatusCode']
+				);
+				$this->sendEmail->sendEmail($params, true);
+			}
+		}
 
 		return [[
 			"status" => 200,
 			"message" => "received"
 		]];
+	}
+
+	private function isRefund($eventType)
+	{
+		return $eventType == 'CHARGE_REFUND_SUCCEEDED';
+	}
+
+	private function executeRefundOrder($order)
+	{
+		return $this->refundOrder->execute($order->getId());
 	}
 }
