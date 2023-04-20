8207f825d37b11a4c11635cc2974c07a9fa74054
Nat-liquido <nataliasantinello@liquido.com>
2023-04-20 12:04:28 -0300

    Adding new methods and conditions for handling the refund response.

--- Model/LiquidoWebhook.php
+++ Model/LiquidoWebhook.php
@@ -13,35 +13,47 @@ use \Magento\Framework\DB\Transaction;
 use \Psr\Log\LoggerInterface;
 
 use \Liquido\PayIn\Helper\LiquidoSalesOrderHelper;
+use \Liquido\PayIn\Helper\LiquidoCreditmemoHelper;
 use \Liquido\PayIn\Helper\LiquidoSendEmail;
+use \Liquido\PayIn\Helper\LiquidoConfigData;
 
 use \LiquidoBrl\PayInPhpSdk\Util\Colombia\PaymentMethod;
+use \LiquidoBrl\PayInPhpSdk\Util\Country;
 
 class LiquidoWebhook
 {
 
 	private Request $request;
 	private LiquidoSalesOrderHelper $liquidoSalesOrderHelper;
+	private LiquidoCreditmemoHelper $liquidoCreditmemoHelper;
 	private LiquidoSendEmail $sendEmail;
 	private ObjectManager $objectManager;
 	private InvoiceService $invoiceService;
 	private Transaction $transaction;
 	private LoggerInterface $logger;
+	private LiquidoDeleteCreditmemo $liquidoDeleteCreditmemo; 
+	private LiquidoConfigData $liquidoConfigData;
 
 	public function __construct(
 		Request $request,
 		LiquidoSalesOrderHelper $liquidoSalesOrderHelper,
+		LiquidoCreditmemoHelper  $liquidoCreditmemoHelper,
 		LiquidoSendEmail $sendEmail,
 		InvoiceService $invoiceService,
 		Transaction $transaction,
-		LoggerInterface $logger
+		LoggerInterface $logger,
+		LiquidoDeleteCreditmemo $liquidoDeleteCreditmemo,
+		LiquidoConfigData $liquidoConfigData
 	) {
 		$this->request = $request;
 		$this->liquidoSalesOrderHelper = $liquidoSalesOrderHelper;
+		$this->liquidoCreditmemoHelper = $liquidoCreditmemoHelper;
 		$this->sendEmail = $sendEmail;
 		$this->invoiceService = $invoiceService;
 		$this->transaction = $transaction;
 		$this->logger = $logger;
+		$this->liquidoDeleteCreditmemo = $liquidoDeleteCreditmemo;
+		$this->liquidoConfigData = $liquidoConfigData;
 		$this->objectManager = ObjectManager::getInstance();
 	}
 
@@ -62,18 +74,16 @@ class LiquidoWebhook
 
 		// if "idempotencyKey" not in $body { do something... }
 		$idempotencyKey = $this->getIdempotencyKey($body);
-
 		$foundLiquidoSalesOrder = $this->liquidoSalesOrderHelper
 			->findLiquidoSalesOrderByIdempotencyKey($idempotencyKey);
 
 		$orderId = $foundLiquidoSalesOrder->getData('order_id');
-
 		$liquidoSalesOrderAlreadyExists = $orderId != null;
 
 		if ($liquidoSalesOrderAlreadyExists) 
 		{
 			// if "transferStatus" not in $body { do something... }
-			$transferStatus = $this->isRefund($eventType) ? 'REFUNDED' : $body["data"]["chargeDetails"]["transferStatus"];
+			$transferStatus = $body["data"]["chargeDetails"]["transferStatus"];
 			$paymentMethod = $body["data"]["chargeDetails"]["paymentMethod"];
 			$orderData = new DataObject(array(
 				"orderId" => $orderId,
@@ -84,8 +94,10 @@ class LiquidoWebhook
 
 			$order = $this->objectManager->create('\Magento\Sales\Model\Order')->loadByIncrementId($orderId);
 
-			if ($transferStatus != 'REFUNDED') 
+			if (!$this->isRefund($eventType)) 
 			{
+				$this->logger->info("###################### IS NOT REFUND ######################");
+
 				if ($order->canInvoice()) 
 				{
 					$invoice = $this->invoiceService->prepareInvoice($order);
@@ -116,6 +128,73 @@ class LiquidoWebhook
 					$this->sendEmail->sendEmail($params, true);
 				}
 			}
+			else
+			{
+				$this->logger->info("###################### IS REFUND ######################");
+
+				$idempotencyKey = $body['data']['chargeDetails']['idempotencyKey'];
+				$foundCreditmemo = $this->liquidoCreditmemoHelper->findCreditmemoByRefundIdempotencyKey($idempotencyKey);
+				$creditmemoId = (int) $foundCreditmemo->getData('creditmemo_id');
+
+				if (array_key_exists('transferStatus', $body['data']['chargeDetails']) && $body['data']['chargeDetails']['transferStatus'] != 'SETTLED')
+				{
+					try {
+						$this->liquidoDeleteCreditmemo->deleteCreditmemo($creditmemoId);
+
+						$creditmemoData = new DataObject(
+							array(
+								"orderId" => $orderId,
+								"creditmemoId" => $foundCreditmemo->getData('creditmemo_id'),
+								"idempotencyKey" => $idempotencyKey,
+								"referenceId" => $body['data']['chargeDetails']['referenceId'],
+								"transferStatus" => "FAILED"
+							)
+						);
+
+						$emailParams = array(
+							'country' => $this->liquidoConfigData->getCountry(),
+							'name' => $body['data']['chargeDetails']['payer']['name'],
+							'email' => $body['data']['chargeDetails']['payer']['email'],
+							'creditmemoId' => $foundCreditmemo->getData('creditmemo_id'),
+							'orderId' => $foundCreditmemo->getData('order_id')
+						);
+
+						$this->sendEmail->sendEmail($emailParams);
+
+						$this->liquidoCreditmemoHelper->createOrUpdateLiquidoCreditmemo($creditmemoData);
+					} catch (\Exception $e) {
+						return [[
+							"status" => 400,
+							"message" => $e
+						]];
+					}
+				}
+				else
+				{
+					try {
+						$creditmemoData = new DataObject(
+							array(
+								"orderId" => $orderId,
+								"creditmemoId" => $foundCreditmemo->getData('creditmemo_id'),
+								"idempotencyKey" => $idempotencyKey,
+								"referenceId" => $body['data']['chargeDetails']['referenceId'],
+								"transferStatus" => "REFUNDED"
+							)
+						);
+						$this->liquidoCreditmemoHelper->createOrUpdateLiquidoCreditmemo($creditmemoData);
+						
+						if ($this->orderIsTotallyRefunded($orderId))
+						{
+							$this->liquidoSalesOrderHelper->updateLiquidoSalesOrderStatus($foundLiquidoSalesOrder, 'REFUNDED');
+						}
+					} catch (\Exception $e) {
+						return [[
+							"status" => 400,
+							"message" => $e
+						]];
+					}
+				}
+			}
 		}
 
 		$this->logger->info("###################### END {$className} processLiquidoCallbackRequest ######################");
@@ -128,7 +207,18 @@ class LiquidoWebhook
 
 	private function isRefund($eventType)
 	{
-		return $eventType === 'CHARGE_REFUND_SUCCEEDED';
+		if ($eventType === 'CHARGE_REFUND_SUCCEEDED')
+		{
+			return true;
+		}
+		elseif ($eventType === 'CHARGE_REFUND_FAILED')
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 
 	private function getIdempotencyKey($bodyInfo)
@@ -138,6 +228,18 @@ class LiquidoWebhook
 		} else {
 			return $bodyInfo["data"]["chargeDetails"]["idempotencyKey"];
 		}
+	}
+
+	private function orderIsTotallyRefunded($orderId)
+	{
+		$bool = false;
+		$order = $this->objectManager->create('\Magento\Sales\Model\Order')->loadByIncrementId($orderId);
+		if ($order->getStatus() == Order::STATE_CLOSED)
+		{
+			$bool = true;
+		}
+
+		return $bool;
 	}
 
 	private function refundOrder($order)
