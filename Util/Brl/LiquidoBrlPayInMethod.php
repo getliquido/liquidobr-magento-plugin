e788b7e26a5126b76335bc07bdb3e5d509d52b51
Nat-liquido <nataliasantinello@liquido.com>
2023-05-23 17:14:05 -0300

    Changing BR images directory.

--- Util/Brl/LiquidoBrlPayInMethod.php
+++ Util/Brl/LiquidoBrlPayInMethod.php
@@ -7,16 +7,11 @@ abstract class LiquidoBrlPayInMethod
     public const PIX = [
         "title" => "Pix",
         "description" => "O pagamento será aprovado na hora.",
-        "image" => "Liquido_PayIn::images/pix.png"
+        "image" => "Liquido_PayIn::images/brl/pix.png"
     ];
     public const BOLETO = [
         "title" => "Boleto",
         "description" => "O pagamento será aprovado em até 3 dias úteis.",
-        "image" => "Liquido_PayIn::images/boleto.png"
+        "image" => "Liquido_PayIn::images/brl/boleto.png"
     ];
-    // public const CREDIT_CARD = [
-    //     "title" => "Cartão de Crédito",
-    //     "description" => "O pagamento poderá ser aprovado na hora.",
-    //     "image" => "Liquido_PayIn::images/credit-card.png"
-    // ];
 }
