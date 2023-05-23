7a9ff2df9db655e3b89bc1040ea96b3218c3c31f
Nat-liquido <nataliasantinello@liquido.com>
2023-05-23 17:16:04 -0300

    Removing Cash and Credit Card constants.

--- Util/Co/LiquidoCoPayInMethod.php
+++ Util/Co/LiquidoCoPayInMethod.php
@@ -11,16 +11,4 @@ abstract class LiquidoCoPayInMethod
         "image" => "Liquido_PayIn::images/col/pse.png",
         "image-selected"=> "Liquido_PayIn::images/col/pse-selected.png",
     ];
-    public const CASH = [
-        "id" => "cash",
-        "title" => "Pago en efectivo",
-        "description" => "El pago puede ser aprobado al instante.",
-        "image" => "Liquido_PayIn::images/col/efecty.png",
-        "image-selected"=> "Liquido_PayIn::images/col/efecty-selected.png",
-    ];
-// public const CREDIT_CARD = [
-//     "title" => "Tarjeta de crédito",
-//     "description" => "El pago puede ser aprobado al instante.",
-//     "image" => "Liquido_PayIn::images/credit-card.png"
-// ];
 }
