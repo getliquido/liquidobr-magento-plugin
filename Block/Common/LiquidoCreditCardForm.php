5f825b8666f4f9b4efde9e91d77c288d776a97ad
Nat-liquido <nataliasantinello@liquido.com>
2023-08-21 15:09:16 -0300

    Getting and validating plans and proposal info.

--- Block/Common/LiquidoCreditCardForm.php
+++ Block/Common/LiquidoCreditCardForm.php
@@ -9,6 +9,9 @@ use \Magento\Checkout\Model\Session;
 use \Liquido\PayIn\Helper\LiquidoConfigData;
 use \Liquido\PayIn\Util\Common\LiquidoPayInMethod;
 use \Liquido\PayIn\Helper\LiquidoOrderData;
+use \Liquido\PayIn\Model\LiquidoPayInSession;
+
+use \LiquidoBrl\PayInPhpSdk\Util\Country;
 
 class LiquidoCreditCardForm extends Template
 {
@@ -16,18 +19,21 @@ class LiquidoCreditCardForm extends Template
     protected Session $checkoutSession;
     private LiquidoOrderData $liquidoOrderData;
     private LiquidoConfigData $liquidoConfig;
+    private LiquidoPayInSession $payInSession;
 
     public function __construct(
         Context $context,
         Session $checkoutSession,
         LiquidoOrderData $liquidoOrderData,
         LiquidoConfigData $liquidoConfig,
+        LiquidoPayInSession $payInSession,
         array $data = []
     ) {
         parent::__construct($context, $data);
         $this->checkoutSession = $checkoutSession;
         $this->liquidoOrderData = $liquidoOrderData;
         $this->liquidoConfig = $liquidoConfig;
+        $this->payInSession = $payInSession;
     }
 
     public function getCardPayInMethodName()
@@ -40,7 +46,7 @@ class LiquidoCreditCardForm extends Template
         return $this->liquidoOrderData->getGrandTotal() / 100;
     }
 
-    public function getCardInstallmentsTextsForOptions()
+    /*public function getCardInstallmentsTextsForOptions()
     {
         $textsForOptionsArray = array();
         $symbol = ($this->getCountry() == 'BR') ? 'R$' : '$';
@@ -65,10 +71,96 @@ class LiquidoCreditCardForm extends Template
         }
 
         return $textsForOptionsArray;
-    }
+    }*/
 
     public function getCountry()
     {
         return $this->liquidoConfig->getCountry();
+    }
+
+    public function getProposalId()
+    {
+        return $this->payInSession->getData('proposalResultData')->getData('id');
+    }
+
+    public function getProposalDetails()
+    {
+        return $this->payInSession->getData('proposalResultData')->getData('proposalDetails');
+    }
+
+    public function getCardInstallmentsTextsForOptions()
+    {
+        $textsForOptionsArray = [];
+        $orderTotal = $this->getOrderTotal();
+        $detailsByCountry = $this->getDetailsByCountry();
+
+        if ($orderTotal >= 1) {
+            if($this->payInSession->getData('proposalResultData')->getData('hasFailed') || ($this->getCountry() == Country::MEXICO || $this->getCountry() == Country::COLOMBIA))
+            {
+                for ($i = $detailsByCountry['init']; $i <= $detailsByCountry['maxInstallments']; $i += $detailsByCountry['increment']) {
+
+                    $installmentAmount = $orderTotal / $i;
+                    $installmentAmountRound = round($installmentAmount, 2);
+                    if (floatval($installmentAmountRound) == 0.01) {
+                        break;
+                    }
+    
+                    $installmentValue = number_format($installmentAmountRound, 2, ',', '.');
+                    $optionInfo = $i . "x de " . $detailsByCountry['currencySymbol'] . $installmentValue;
+                    array_push($textsForOptionsArray, $optionInfo);
+
+                    if($this->getCountry() == Country::MEXICO && $i == 12)
+                    {
+                        $detailsByCountry['increment'] = 6;
+                    }
+                }
+            }
+            else
+            {
+                $proposalDetails = $this->getProposalDetails();
+                foreach($proposalDetails as $optionDetail){
+                    $installmentAmount = $optionDetail->installmentAmount / 100;
+                    $installmentValue = number_format($installmentAmount, 2, ',', '.');
+                    $optionInfo = $optionDetail->installments . "x de " . $detailsByCountry['currencySymbol'] . $installmentValue;
+                    array_push($textsForOptionsArray, $optionInfo);
+                }
+            }
+        } else {
+            $orderTotal = number_format($orderTotal, 2, ',', '.');
+            $optionInfo = "1 x de {$detailsByCountry['currencySymbol']} {$orderTotal}";
+            array_push($textsForOptionsArray, $optionInfo);
+        }
+        
+        return $textsForOptionsArray;
+    }
+
+    public function getDetailsByCountry()
+    {
+        switch ($this->getCountry()) {
+            case Country::BRAZIL:
+                return [
+                    'currencySymbol' => 'R$',
+                    'maxInstallments' => 12,
+                    'increment' => 1,
+                    'init' => 1
+                ];
+                break;
+            case Country::MEXICO:
+                return [
+                    'currencySymbol' => '$',
+                    'maxInstallments' => 18,
+                    'increment' => 3,
+                    'init' => 3
+                ];
+                break;            
+            case Country::COLOMBIA:
+                return [
+                        'currencySymbol' => '$',
+                        'maxInstallments' => 12,
+                        'increment' => 1,
+                        'init' => 1
+                    ];
+                break;
+        }
     }
 }
--- Controller/Liquido/CreditCardForm.php
+++ Controller/Liquido/CreditCardForm.php
@@ -4,19 +4,112 @@ namespace Liquido\PayIn\Controller\Liquido;
 
 use \Magento\Framework\App\ActionInterface;
 use \Magento\Framework\View\Result\PageFactory;
+use \Magento\Framework\DataObject;
+
+use \Liquido\PayIn\Helper\LiquidoOrderData;
+use \Liquido\PayIn\Helper\LiquidoConfigData;
+use \Liquido\PayIn\Logger\Logger;
+use \Liquido\PayIn\Model\LiquidoPayInSession;
+
+use \LiquidoBrl\PayInPhpSdk\Util\Config;
+use \LiquidoBrl\PayInPhpSdk\Util\Country;
+use \LiquidoBrl\PayInPhpSdk\Util\Currency;
+use \LiquidoBrl\PayInPhpSdk\Model\PayInRequest;
+use \LiquidoBrl\PayInPhpSdk\Service\PayInService;
 
 class CreditCardForm implements ActionInterface
 {
     private PageFactory $resultPageFactory;
+    private DataObject $proposalResultData;
+    private PayInService $payInService;
+    private Logger $logger;
+    private LiquidoConfigData $liquidoConfig;
+    protected LiquidoPayInSession $payInSession;
+    private LiquidoOrderData $liquidoOrderData;
 
     public function __construct(
-        PageFactory $resultPageFactory
+        PageFactory $resultPageFactory,
+        PayInService $payInService,
+        Logger $logger,
+        LiquidoPayInSession $payInSession,
+        LiquidoConfigData $liquidoConfig,
+        LiquidoOrderData $liquidoOrderData
     ) {
         $this->resultPageFactory = $resultPageFactory;
+        $this->proposalResultData = new DataObject(array());
+        $this->payInService = $payInService;
+        $this->logger = $logger;
+        $this->payInSession = $payInSession;
+        $this->liquidoConfig = $liquidoConfig;
+        $this->liquidoOrderData = $liquidoOrderData;
     }
 
     public function execute()
     {
+        $className = static::class;
+        $this->logger->info("###################### BEGIN ######################");
+        $this->logger->info("[ {$className} Controller ]: Credit Card Form Request received.");
+
+        $this->proposalResultData = new DataObject(array(
+            'id' => null,
+            'proposalDetails' => [],
+            'hasFailed' => false
+        ));
+
+        $config = new Config(
+            [
+                'clientId' => $this->liquidoConfig->getClientId(),
+                'clientSecret' => $this->liquidoConfig->getClientSecret(),
+                'apiKey' => $this->liquidoConfig->getApiKey()
+            ],
+            $this->liquidoConfig->isProductionModeActived()
+        );
+
+        try {
+            $paymentPlanResponse = $this->payInService->getInstallmentPlans($config);
+            $countryAndCurrency = $this->getCountryAndCurrency();
+
+            $planRequest = new PayInRequest([
+                "amount" => $this->liquidoOrderData->getGrandTotal(),    
+                "currency" => $countryAndCurrency["currency"],
+                "country" => $countryAndCurrency["country"],
+                "installmentPlanId" => $paymentPlanResponse[0]->id
+            ]);
+
+            $proposalResponse = $this->payInService->createProposal($config, $planRequest);
+
+            $this->proposalResultData->setData('id', $proposalResponse->id);
+            $this->proposalResultData->setData('proposalDetails', $proposalResponse->proposalDetails->installmentProposalDetails);
+        } catch (\Exception $e) {
+            $this->proposalResultData->setData('hasFailed', true);
+        }
+
+        $this->payInSession->setData('proposalResultData', $this->proposalResultData);
+
         return $this->resultPageFactory->create();
+    }
+
+    public function getCountryAndCurrency()
+    {
+        switch ($this->liquidoConfig->getCountry()) {
+            case Country::BRAZIL:
+                return [
+                    'country' => Country::BRAZIL,
+                    'currency' => Currency::BRL
+                ];
+                break;
+            case Country::COLOMBIA:
+                return [
+                    'country' => Country::COLOMBIA,
+                    'currency' => Currency::COP
+                ];
+                break;
+            case Country::MEXICO:
+                return [
+                    'country' => Country::MEXICO,
+                    'currency' => Currency::MXN
+                ];
+                break;
+        }
     }
 }
